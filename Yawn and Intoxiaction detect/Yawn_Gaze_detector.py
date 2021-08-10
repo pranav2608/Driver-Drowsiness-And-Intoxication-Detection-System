a0f33ccfc691545b16842d7b1a73cf426eafa64b
Pranav Mahajan <mahajanpranav41@gmail.com>
2021-08-10 13:37:49 +0530

    final commit

---
+++ Yawn and Intoxiaction detect/Yawn_Gaze_detector.py
@@ -0,0 +1,136 @@
+"""
+Created on Tue May 18 13:20:25 2021
+
+@author: ASUS
+"""
+
+import cv2
+from gaze_tracking import GazeTracking
+import dlib
+import numpy as np
+
+PREDICTOR_PATH = "E:/AI & ML Project/shape_predictor_68_face_landmarks.dat"
+predictor = dlib.shape_predictor(PREDICTOR_PATH)
+cascade_path='E:/AI & ML Project/haarcascade_frontalface_default.xml'
+cascade = cv2.CascadeClassifier(cascade_path)
+detector = dlib.get_frontal_face_detector()
+
+
+def get_landmarks(im):
+    rects = detector(im, 1)
+
+    if len(rects) > 1:
+        return "error"
+    if len(rects) == 0:
+        return "error"
+    return np.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
+
+
+def annotate_landmarks(im, landmarks):
+    im = im.copy()
+    for idx, point in enumerate(landmarks):
+        pos = (point[0, 0], point[0, 1])
+        cv2.putText(im, str(idx), pos,
+                    fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+                    fontScale=0.4,
+                    color=(0, 0, 255))
+        cv2.circle(im, pos, 3, color=(0, 255, 255))
+    return im
+
+def top_lip(landmarks):
+    top_lip_pts = []
+    for i in range(50,53):
+        top_lip_pts.append(landmarks[i])
+    for i in range(61,64):
+        top_lip_pts.append(landmarks[i])
+    top_lip_all_pts = np.squeeze(np.asarray(top_lip_pts))
+    top_lip_mean = np.mean(top_lip_pts, axis=0)
+    return int(top_lip_mean[:,1])
+
+def bottom_lip(landmarks):
+    bottom_lip_pts = []
+    for i in range(65,68):
+        bottom_lip_pts.append(landmarks[i])
+    for i in range(56,59):
+        bottom_lip_pts.append(landmarks[i])
+    bottom_lip_all_pts = np.squeeze(np.asarray(bottom_lip_pts))
+    bottom_lip_mean = np.mean(bottom_lip_pts, axis=0)
+    return int(bottom_lip_mean[:,1])
+
+def mouth_open(image):
+    landmarks = get_landmarks(image)
+    
+    if landmarks == "error":
+        return image, 0
+    
+    image_with_landmarks = annotate_landmarks(image, landmarks)
+    top_lip_center = top_lip(landmarks)
+    bottom_lip_center = bottom_lip(landmarks)
+    lip_distance = abs(top_lip_center - bottom_lip_center)
+    return image_with_landmarks, lip_distance
+
+    cv2.imshow('Result', image_with_landmarks)
+    cv2.imwrite('image_with_landmarks.jpg',image_with_landmarks)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+yawns = 0
+yawn_status = False 
+gaze = GazeTracking()
+webcam = cv2.VideoCapture(0)
+
+while True:
+    # We get a new frame from the webcam
+    _, frame = webcam.read()
+
+    # We send this frame to GazeTracking to analyze it
+    gaze.refresh(frame)
+
+    frame = gaze.annotated_frame()
+    text = ""
+    cnt=0
+    if gaze.is_blinking():
+        text = "Blinking"
+    if gaze.is_center():
+        text = "Chances of Intoxication"
+            
+        
+
+    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+
+    left_pupil = gaze.pupil_left_coords()
+    right_pupil = gaze.pupil_right_coords()
+    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+     
+    ret, frame_1 = webcam.read()   
+    image_landmarks, lip_distance = mouth_open(frame_1)
+    
+    prev_yawn_status = yawn_status  
+    
+    if lip_distance > 25:
+        yawn_status = True 
+        
+        cv2.putText(frame_1, "Subject is Yawning", (50,450), 
+                    cv2.FONT_HERSHEY_COMPLEX, 1,(0,0,255),2)
+        
+
+        output_text = " Yawn Count: " + str(yawns + 1)
+
+        cv2.putText(frame_1, output_text, (50,50),
+                    cv2.FONT_HERSHEY_COMPLEX, 1,(0,255,127),2)
+        
+    else:
+        yawn_status = False 
+         
+    if prev_yawn_status == True and yawn_status == False:
+        yawns += 1
+
+   
+    cv2.imshow('Yawn Detection', frame_1 )
+    
+    cv2.imshow("Demo", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
